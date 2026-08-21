f0c65a6d664fa202f25bca636894486f5face7eb
ilhamrf540-ship-it <ilhamrf540-ship-it@example.com>
2026-08-21 11:54:05 +0700

    Configure HiveMQ broker and display phone numbers in user profiles

--- app.js
+++ app.js
@@ -1484,7 +1484,26 @@ let rtcIceQueue = [];
 const rtcConfig = {
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
-    { urls: 'stun:stun1.l.google.com:19302' }
+    { urls: 'stun:stun1.l.google.com:19302' },
+    { urls: 'stun:stun2.l.google.com:19302' },
+    { urls: 'stun:stun3.l.google.com:19302' },
+    { urls: 'stun:stun4.l.google.com:19302' },
+    // Free public TURN/STUN servers from OpenRelayProject to traverse symmetric NATs / Firewalls
+    {
+      urls: 'turn:openrelay.metered.ca:80',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    }
   ]
 };
 
